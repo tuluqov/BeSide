504f1b4da6c9034c684fad214067911deab334ca
Yusup9505 <vashi.abov@mail.ru>
2018-07-16 11:27:09 +0300

    Add error page and accont property

---
+++ BeSide.Common.DependencyInjection/DataAccessModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using Autofac.Core;
+using Autofac.Integration.Mvc;
+using System.Web.Mvc;
+
+namespace BeSide.Common.DependencyInjection
+{
+    public class DataAccessModule
+    {
+        public static void Configurate()
+        {
+            var builder = new ContainerBuilder();
+
+
+        }
+    }
+}
